34a7a0d511a0ffc49059569ef66fd101eea6f186
John Xu <john.xud@gmail.com>
2021-05-13 19:01:29 +1000

    new constructor param

--- test/test.js
+++ test/test.js
@@ -1,13 +1,11 @@
 "use strict";
-const aws = require('aws-sdk');
 const configReader = require('../index');
 
-let test = async () => {
-    aws.config.update({
-        region: 'ap-southeast-2',
-    });
-    let cr = new configReader();
-    let params = await cr.read('/Test');
-    console.log(params);
+const cr = new configReader();
+
+async function sampleFunction(path) {
+    let config = await cr.read(path);
+    console.log(config);
 }
-test();
+
+sampleFunction('/test');
